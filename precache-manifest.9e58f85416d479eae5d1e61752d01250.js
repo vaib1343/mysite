c8739b0a4445106c22add8df028577e831d3391c
vaib1343 <vaib1343@gmail.com>
2021-11-07 09:03:02 +0530

    Updates

---
+++ precache-manifest.9e58f85416d479eae5d1e61752d01250.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3653b2780f5c13b1bfcaba2fce2e0a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24edbb891530bbffc249",
+    "url": "/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "cd1902f1cf09a7934c88",
+    "url": "/static/css/main.abb13a0e.chunk.css"
+  },
+  {
+    "revision": "24edbb891530bbffc249",
+    "url": "/static/js/2.05a71abe.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/static/js/2.05a71abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd1902f1cf09a7934c88",
+    "url": "/static/js/main.2928bb4a.chunk.js"
+  },
+  {
+    "revision": "4d48fb175f530d8eba22",
+    "url": "/static/js/runtime-main.665cd873.js"
+  },
+  {
+    "revision": "f4893935a7f777feaa5a9164bff3e95e",
+    "url": "/static/media/aboutimage.f4893935.jpg"
+  },
+  {
+    "revision": "fbfac211a96785e70389423643dd0b09",
+    "url": "/static/media/contactimage.fbfac211.jpg"
+  },
+  {
+    "revision": "bb51b869b709212f4e55815ca826d9f1",
+    "url": "/static/media/headerimage.bb51b869.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e3a129fc66ab89df97a28a4c63da1b0e",
+    "url": "/static/media/meter.e3a129fc.jpg"
+  }
+]);
